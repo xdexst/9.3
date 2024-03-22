ac096d97cf8cc627e915588883b717a78847b413
xdexst <rnoy10303@gmail.com>
2024-03-22 22:22:58 +0400

    Реализация класса MainWindow в котором создаются и настриваются таймер и виджет

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,24 @@
 #include "mainwindow.h"
-#include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
+    this->setFixedWidth(300);
+    this->setFixedHeight(200);
+    Label1 = new QLabel(this);
+    Label1->setGeometry(20,20,100,20);
+    Label1->setText("Test v9.2: OK.");
+    horizbtn = new QPushButton(this);
+    vertbtn = new QPushButton(this);
+
+    timer = new QTimer(this);
+
+    horizbtn->setText("Horiz");
+    vertbtn->setText("Vert");
+    horizbtn->setGeometry(240,140,50,20);
+    vertbtn->setGeometry(240,170,50,20);
+
+    connect(horizbtn, &QPushButton::clicked, this, &MainWindow::horiz);
+    connect(vertbtn, &QPushButton::clicked, this, &MainWindow::vert);
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
+MainWindow::~MainWindow() {
 }
-
